d24416c73c3a6f08ee15df2131dc84f7d3b98de8
e.utrobin <e.utrobin@corp.mail.ru>
2017-10-29 23:26:56 +0300

    убрал нижние меню сделал боковое

--- application/components/App/App.tsx
+++ application/components/App/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { withStyles } from 'material-ui/styles';
-import BottomNavigation, {BottomNavigationButton} from 'material-ui/BottomNavigation';
-import {Search, AccountCircle, LocationOn} from 'material-ui-icons';
+import {BottomNavigationButton} from 'material-ui/BottomNavigation';
+import {Search, AccountCircle, LocationOn, Announcement} from 'material-ui-icons';
+import List, { ListItem, ListItemIcon, ListItemText } from 'material-ui/List';
+import {AppBar, Toolbar, IconButton, Drawer, Divider} from 'material-ui';
+import Typography from 'material-ui/Typography';
+import MenuIcon from 'material-ui-icons/Menu';
 import BaseCart from '../BaseCart/BaseCart';
 
-const styles = {
+const styles = theme => ({
 	root: {
 		width: 500,
 	},
+
+	list: {
+		width: 250,
+		background: theme.palette.background.paper,
+	},
+
 	bottomNavigation: {
 		position: 'fixed',
 		bottom: '-500px',
@@ -17,12 +27,25 @@ const styles = {
 		display: 'flex',
 		justifyContent: 'center',
 		boxShadow: '10px -5px 15px -7px rgba(0,0,0,0.55)',
-	}
-};
+	},
+	menuButton: {
+		marginLeft: -12,
+		marginRight: 20,
+	},
+	flex: {
+		flex: 1,
+	},
+});
 
-class App extends React.Component<any> {
+class App extends React.Component<any, any> {
 	state = {
-		value: 'search',
+		left: false,
+	};
+
+	toggleDrawer = (side, open) => () => {
+		this.setState({
+			[side]: open,
+		});
 	};
 
 	handleChange = (event, value) => {
@@ -31,22 +54,71 @@ class App extends React.Component<any> {
 
 	render() {
 		const { classes } = this.props;
-		const { value } = this.state;
+
+		const sideList = (
+			<div className={classes.list}>
+				<List>
+					<ListItem button>
+						<ListItemIcon>
+							<Search />
+						</ListItemIcon>
+						<ListItemText primary="Поиск" />
+					</ListItem>
+					<ListItem button>
+						<ListItemIcon>
+							<LocationOn />
+						</ListItemIcon>
+						<ListItemText primary="Карта" />
+					</ListItem>
+					<ListItem button>
+						<ListItemIcon>
+							<AccountCircle />
+						</ListItemIcon>
+						<ListItemText primary="Мой профиль" />
+					</ListItem>
+				</List>
+				<Divider />
+				<List>
+					<ListItem button>
+						<ListItemIcon>
+							<Announcement />
+						</ListItemIcon>
+						<ListItemText primary="Оставить отзыв" />
+					</ListItem>
+				</List>
+			</div>
+		);
 
 		return (
-			<div style={{display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap'}}>
-				{
-					[1,2,3,4,5,6].map((el) =>
-						<BaseCart key={el} />
-					)
-				}
+			<div>
+				<AppBar>
+					<Toolbar>
+						<IconButton className={classes.menuButton} color="contrast" aria-label="Menu"  onClick={this.toggleDrawer('left', true)}>
+							<MenuIcon />
+						</IconButton>
+						<Typography type="title" color="inherit" className={classes.flex}>
+							Луна
+						</Typography>
+					</Toolbar>
+				</AppBar>
 
-				<div className={classes.bottomNavigation}>
-					<BottomNavigation value={value} onChange={this.handleChange} className={classes.root}>
-						<BottomNavigationButton label="Поиск" value="search" icon={<Search />} />
-						<BottomNavigationButton label="Карта" value="map" icon={<LocationOn />} />
-						<BottomNavigationButton label="Мой профиль" value="profile" icon={<AccountCircle />} />
-					</BottomNavigation>
+				<Drawer open={this.state.left} onRequestClose={this.toggleDrawer('left', false)}>
+					<div
+						tabIndex={0}
+						role="button"
+						onClick={this.toggleDrawer('left', false)}
+						onKeyDown={this.toggleDrawer('left', false)}
+					>
+						{sideList}
+					</div>
+				</Drawer>
+
+				<div style={{display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', paddingTop: 80}}>
+					{
+						[1,2,3,4,5,6].map((el) =>
+							<BaseCart key={el} />
+						)
+					}
 				</div>
 			</div>
 		);
